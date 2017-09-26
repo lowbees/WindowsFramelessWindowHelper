547e05f137b3a5ec27c63f323bc241909d84f3d2
lowbees <demoresh@126.com>
2017-09-26 20:59:06 +0800

    修改析构函数为virtual，避免内存泄漏

--- FramelessHelper/basicframelesshelper.h
+++ FramelessHelper/basicframelesshelper.h
@@ -16,7 +16,7 @@ class BasicFramelessHelper : public QObject
     Q_PROPERTY(int bottomResizeMargin READ bottomResizeMargin  WRITE setBottomResizeMargin NOTIFY bottomResizeMarginChanged)
 public:
     explicit BasicFramelessHelper(QObject *parent = 0);
-    ~BasicFramelessHelper();
+    virtual ~BasicFramelessHelper();
 public:
     virtual bool nativeEventFilter(void *message, long *result) = 0;
 
@@ -24,53 +24,57 @@ public:
     Q_INVOKABLE void addTitleObjects(const QObjectList &objs);
     Q_INVOKABLE void addTitleObject(QObject *obj);
 
-    Q_INVOKABLE int titleHeight() const {
+    int titleHeight() const {
         return m_titleHeight;
     }
-    Q_INVOKABLE void setTitleHeight(int h) {
+    
+    int leftResizeMargin()  const {
+        return m_leftResizeMargin;
+    }
+
+    int topResizeMargin()  const {
+        return m_topResizeMargin;
+    }
+    
+    int rightResizeMargin()  const {
+        return m_rightResizeMargin;
+    }
+
+    int bottomResizeMargin()  const {
+        return m_leftResizeMargin;
+    }
+    
+public slots:
+    void setTitleHeight(int h) {
         if (h >= 0 && h != m_titleHeight) {
             m_titleHeight = h;
             titleHeightChanged(h);
         }
     }
-    Q_INVOKABLE int leftResizeMargin()  const {
-        return m_leftResizeMargin;
-    }
-    Q_INVOKABLE void setLeftResizeMargin(int m) {
+    void setLeftResizeMargin(int m) {
         if (m >= 0 && m != m_leftResizeMargin) {
             m_leftResizeMargin = m;
             leftResizeMarginChanged(m);
         }
     }
-
-    Q_INVOKABLE int topResizeMargin()  const {
-        return m_topResizeMargin;
-    }
-    Q_INVOKABLE void setTopResizeMargin(int m) {
+    void setTopResizeMargin(int m) {
         if (m >= 0 && m != m_topResizeMargin) {
             m_topResizeMargin = m;
             topResizeMarginChanged(m);
         }
     }
-    Q_INVOKABLE int rightResizeMargin()  const {
-        return m_rightResizeMargin;
-    }
-    Q_INVOKABLE void setRightResizeMargin(int m) {
+    void setRightResizeMargin(int m) {
         if (m >= 0 && m != m_rightResizeMargin) {
             m_rightResizeMargin = m;
             rightResizeMarginChanged(m);
         }
     }
-    Q_INVOKABLE int bottomResizeMargin()  const {
-        return m_leftResizeMargin;
-    }
-    Q_INVOKABLE void setBottomResizeMargin(int m) {
+    void setBottomResizeMargin(int m) {
         if (m >= 0 && m != m_bottomResizeMargin) {
             m_bottomResizeMargin = m;
             bottomResizeMarginChanged(m);
         }
     }
-
 signals:
     void titleHeightChanged(int);
     void leftResizeMarginChanged(int);
